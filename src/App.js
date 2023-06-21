0f24aa284e839da9b3e21195ebde7ade52fe1010
leelamanikanta01 <leelamanikanta01@gmail.com>
2023-06-21 16:40:03 +0000

    Assign

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import {Component} from 'react'
+
+import {Route, Switch} from 'react-router-dom'
+import Register from './components/Register'
+import Homepage from './components/Homepage'
+
+import RegisterContext from './context/RegisterContext'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    name: '',
+    topic: 'Arts and Culture',
+    isRegistered: false,
+    showError: false,
+  }
+
+  changeName = name => {
+    this.setState({name})
+  }
+
+  changeTopic = topic => {
+    this.setState({topic})
+  }
+
+  registerName = () => {
+    this.setState({isRegistered: true})
+  }
+
+  updateError = () => {
+    this.setState({showError: true})
+  }
+
+  render() {
+    const {name, topic, isRegistered, showError} = this.state
+    return (
+      <RegisterContext.Provider
+        value={{
+          name,
+          topic,
+          isRegistered,
+          showError,
+          changeName: this.changeName,
+          changeTopic: this.changeTopic,
+          registerName: this.registerName,
+          updateError: this.updateError,
+        }}
+      >
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </RegisterContext.Provider>
+    )
+  }
+}
+
+export default App
